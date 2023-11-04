117fb5d7b3bbf54efb50cf2262a0e6d4f4023a3f
h31l0k1tty <132772150+h31l0k1tty@users.noreply.github.com>
2023-11-04 18:06:23 +0300

    AuthPage & RegPages XAML

---
+++ MyCashier/MyCashier/Pages/AuthPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyCashier.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthPage.xaml
+    /// </summary>
+    public partial class AuthPage : Page
+    {
+        public AuthPage()
+        {
+            InitializeComponent();
+        }
+        private void regButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void authButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void rememberCheckBox_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
