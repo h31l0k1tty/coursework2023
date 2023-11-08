dbaa66a4bf393ef07191ef9d8490032d189f6be0
h31l0k1tty <semyonovvaofficial@gmail.com>
2023-11-08 10:11:03 +0300

    added AddAccount View

---
+++ MyCashier/MyCashier/MVVM/Views/UC_AddAccount.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyCashier.MVVM.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для UC_AddAccount.xaml
+    /// </summary>
+    public partial class UC_AddAccount : Page
+    {
+        public UC_AddAccount()
+        {
+            InitializeComponent();
+        }
+    }
+}
